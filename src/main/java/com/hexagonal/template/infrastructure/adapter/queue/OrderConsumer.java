4fd4895f4a1842fa46c8b5acfa45f37c7c5f67e0
Rafael Araújo <rafaelaraujosilva@gmail.com>
2025-05-19 07:49:43 -0300

    ajustando configurações da Mensageria

--- src/main/java/com/hexagonal/template/infrastructure/adapter/queue/OrderConsumer.java
+++ src/main/java/com/hexagonal/template/infrastructure/adapter/queue/OrderConsumer.java
@@ -1,6 +1,9 @@
 package com.hexagonal.template.infrastructure.adapter.queue;
 
+import com.google.gson.Gson;
 import com.hexagonal.template.application.service.orchestration.OrderQueueService;
+import com.hexagonal.template.domain.exception.BusinessException;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,8 +14,16 @@ public class OrderConsumer {
     @Autowired
     private OrderQueueService service;
 
-    @RabbitListener(queues = "INPUT_ORDER")
+    @RabbitListener(queues = "${spring.rabbitmq.queue.order.input}")
     public void receiveOrderMessage(String message) {
-        service.create(message);
+        try {
+            service.create(message);
+        }
+        catch (BusinessException e) {
+                throw new AmqpRejectAndDontRequeueException("Erro de negócio: " + new Gson().toJson(e.getErrorResponse()));
+        }
+        catch (Exception e) {
+                throw new AmqpRejectAndDontRequeueException("Internal Server Error: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/hexagonal/template/infrastructure/adapter/queue/OrderProducer.java
+++ src/main/java/com/hexagonal/template/infrastructure/adapter/queue/OrderProducer.java
@@ -3,6 +3,7 @@ package com.hexagonal.template.infrastructure.adapter.queue;
 import com.hexagonal.template.domain.port.queue.OrderPort;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +12,15 @@ public class OrderProducer implements OrderPort {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Value("${spring.rabbitmq.queue.order.output}")
+    public String OUTPUT_ORDER_QUEUE;
+
     public void sendOrderMessage(String message) {
-        rabbitTemplate.convertAndSend("OUTPUT_ORDER", message);
+        try {
+            rabbitTemplate.convertAndSend(OUTPUT_ORDER_QUEUE, message);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }

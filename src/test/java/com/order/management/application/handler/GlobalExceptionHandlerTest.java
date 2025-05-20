cc9b03cb89117f433bb2a2a43b7e332cec17c360
Rafael Araújo <rafaelaraujosilva@gmail.com>
2025-05-19 22:36:07 -0300

    inserindo classe nova de teste GlobalExceptionHandlerTest.java

--- src/test/java/com/order/management/application/handler/GlobalExceptionHandlerTest.java
+++ src/test/java/com/order/management/application/handler/GlobalExceptionHandlerTest.java
@@ -1,211 +1,206 @@
-//package com.order.management.application.handler;
-//
-//import com.order.management.domain.exception.BusinessException;
-//import com.order.management.domain.model.error.ErrorResponse;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.validation.FieldError;
-//import org.springframework.web.bind.MethodArgumentNotValidException;
-//
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.when;
-//
-//@ExtendWith(MockitoExtension.class)
-//class GlobalExceptionHandlerTest {
-//
-//    @InjectMocks
-//    private GlobalExceptionHandler handler;
-//
-//    @Mock
-//    private BusinessException businessException;
-//
-//    @Mock
-//    private MethodArgumentNotValidException validationException;
-//
-//    @Mock
-//    private BindingResult bindingResult;
-//
-//    @Test
-//    void handleBusinessException_returnsResponseEntityWithErrorResponseAndStatus() {
-//
-//        ErrorResponse errorResponse = ErrorResponse.builder()
-//                .status(409)
-//                .title("Conflict Error")
-//                .description("Resource already exists")
-//                .build();
-//        when(businessException.getErrorResponse()).thenReturn(errorResponse);
-//
-//        ResponseEntity<ErrorResponse> response = handler.handleBusinessException(businessException);
-//
-//        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
-//        assertEquals(errorResponse, response.getBody());
-//    }
-//
-//    @Test
-//    void handleBusinessException_returnsInternalServerErrorIfStatusInvalid() {
-//
-//        ErrorResponse errorResponse = ErrorResponse.builder()
-//                .status(999)
-//                .title("Invalid Status Error")
-//                .description("Invalid status provided")
-//                .build();
-//        when(businessException.getErrorResponse()).thenReturn(errorResponse);
-//
-//        ResponseEntity<ErrorResponse> response = handler.handleBusinessException(businessException);
-//
-//        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-//        assertEquals(errorResponse, response.getBody());
-//    }
-//
-//    @Test
-//    void handleValidationExceptions_singleNotNullError_returnsBadRequestWithErrorResponse() {
-//
-//        FieldError notNullError = new FieldError("orderRequest", "customerId", "must not be null");
-//        when(validationException.getBindingResult()).thenReturn(bindingResult);
-//        when(bindingResult.getAllErrors()).thenReturn(List.of(notNullError));
-//
-//        ResponseEntity<ErrorResponse> response = handler.handleValidationExceptions(validationException);
-//
-//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-//        assertEquals("Erro na validação de campos", response.getBody().getTitle());
-//        assertEquals("must not be null", response.getBody().getDescription());
-//        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getBody().getStatus());
-//    }
-//
-//    @Test
-//    void handleValidationExceptions_singleNotEmptyError_returnsBadRequestWithErrorResponse() {
-//
-//        FieldError notEmptyError = new FieldError("orderRequest", "products", "must not be empty");
-//        when(validationException.getBindingResult()).thenReturn(bindingResult);
-//        when(bindingResult.getAllErrors()).thenReturn(List.of(notEmptyError));
-//
-//        ResponseEntity<ErrorResponse> response = handler.handleValidationExceptions(validationException);
-//
-//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-//        assertEquals("Erro na validação de campos", response.getBody().getTitle());
-//        assertEquals("must not be empty", response.getBody().getDescription());
-//        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getBody().getStatus());
-//    }
-//
-//    @Test
-//    void handleValidationExceptions_singleNotBlankError_returnsBadRequestWithErrorResponse() {
-//
-//        FieldError notBlankError = new FieldError("orderRequest", "notes", "must not be blank");
-//        when(validationException.getBindingResult()).thenReturn(bindingResult);
-//        when(bindingResult.getAllErrors()).thenReturn(List.of(notBlankError));
-//
-//        ResponseEntity<ErrorResponse> response = handler.handleValidationExceptions(validationException);
-//
-//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-//        assertEquals("Erro na validação de campos", response.getBody().getTitle());
-//        assertEquals("must not be blank", response.getBody().getDescription());
-//        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getBody().getStatus());
-//    }
-//
-//    @Test
-//    void handleValidationExceptions_multipleErrors_returnsBadRequestWithErrorResponseBasedOnPrecedence() {
-//
-//        FieldError notNullError = new FieldError("orderRequest", "customerId", "must not be null");
-//        FieldError notEmptyError = new FieldError("orderRequest", "products", "must not be empty");
-//        FieldError notBlankError = new FieldError("orderRequest", "notes", "must not be blank");
-//        when(validationException.getBindingResult()).thenReturn(bindingResult);
-//        when(bindingResult.getAllErrors()).thenReturn(List.of(notNullError, notEmptyError, notBlankError));
-//
-//        ResponseEntity<ErrorResponse> response = handler.handleValidationExceptions(validationException);
-//
-//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-//        assertEquals("Erro na validação de campos", response.getBody().getTitle());
-//        assertEquals("must not be null", response.getBody().getDescription());
-//        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getBody().getStatus());
-//    }
-//
-//    @Test
-//    void mapError_mapsFieldErrorToErrorResponse() {
-//
-//        Map<String, ErrorResponse> errors = new HashMap<>();
-//        FieldError fieldError = new FieldError("orderRequest", "quantity", "must be greater than 0");
-//
-//        handler.mapError(fieldError, errors);
-//
-//        assertEquals(1, errors.size());
-//        assertEquals("Erro na validação de campos", errors.get("Min").getTitle());
-//        assertEquals("must be greater than 0", errors.get("Min").getDescription());
-//        assertEquals(HttpStatus.BAD_REQUEST.value(), errors.get("Min").getStatus());
-//    }
-//
-//    @Test
-//    void returnError_containsKeyNotNullOnly_returnsNotNullError() {
-//
-//        Map<String, ErrorResponse> errors = new HashMap<>();
-//        ErrorResponse notNullErrorResponse = ErrorResponse.builder().description("NotNull error").build();
-//        errors.put("NotNull", notNullErrorResponse);
-//
-//        ErrorResponse result = handler.returnError(errors);
-//
-//        assertEquals(notNullErrorResponse, result);
-//    }
-//
-//    @Test
-//    void returnError_containsKeyNotNullNotEmptyNotBlank_returnsNotNullError() {
-//
-//        Map<String, ErrorResponse> errors = new HashMap<>();
-//        ErrorResponse notNullErrorResponse = ErrorResponse.builder().description("NotNull error").build();
-//        ErrorResponse notEmptyErrorResponse = ErrorResponse.builder().description("NotEmpty error").build();
-//        ErrorResponse notBlankErrorResponse = ErrorResponse.builder().description("NotBlank error").build();
-//        errors.put("NotNull", notNullErrorResponse);
-//        errors.put("NotEmpty", notEmptyErrorResponse);
-//        errors.put("NotBlank", notBlankErrorResponse);
-//
-//        ErrorResponse result = handler.returnError(errors);
-//
-//        assertEquals(notNullErrorResponse, result);
-//    }
-//
-//    @Test
-//    void returnError_containsKeyNotEmptyNotBlankOnly_returnsNotEmptyError() {
-//
-//        Map<String, ErrorResponse> errors = new HashMap<>();
-//        ErrorResponse notEmptyErrorResponse = ErrorResponse.builder().description("NotEmpty error").build();
-//        ErrorResponse notBlankErrorResponse = ErrorResponse.builder().description("NotBlank error").build();
-//        errors.put("NotEmpty", notEmptyErrorResponse);
-//        errors.put("NotBlank", notBlankErrorResponse);
-//
-//        ErrorResponse result = handler.returnError(errors);
-//
-//        assertEquals(notEmptyErrorResponse, result);
-//    }
-//
-//    @Test
-//    void returnError_containsKeyNotBlankOnly_returnsNotBlankError() {
-//
-//        Map<String, ErrorResponse> errors = new HashMap<>();
-//        ErrorResponse notBlankErrorResponse = ErrorResponse.builder().description("NotBlank error").build();
-//        errors.put("NotBlank", notBlankErrorResponse);
-//
-//        ErrorResponse result = handler.returnError(errors);
-//
-//        assertEquals(notBlankErrorResponse, result);
-//    }
-//
-//    @Test
-//    void returnError_containsNoPriorityKeys_returnsDefaultErrorResponse() {
-//
-//        Map<String, ErrorResponse> errors = new HashMap<>();
-//        ErrorResponse otherErrorResponse = ErrorResponse.builder().description("Some other error").build();
-//        errors.put("Min", otherErrorResponse);
-//
-//        ErrorResponse result = handler.returnError(errors);
-//
-//        assertEquals(new ErrorResponse(), result);
-//    }
-//}
+package com.order.management.application.handler;
+
+import com.order.management.domain.exception.BusinessException;
+import com.order.management.domain.model.error.ErrorResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class GlobalExceptionHandlerTest {
+
+    @InjectMocks
+    private GlobalExceptionHandler handler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void handleBusinessException() {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .title("Business Error")
+                .description("Business rule violated")
+                .status(400)
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
+        BusinessException businessException = new BusinessException(errorResponse);
+
+        ResponseEntity<ErrorResponse> response = handler.handleBusinessException(businessException);
+
+        assertEquals(400, response.getStatusCode().value());
+        assertEquals("Business Error", response.getBody().getTitle());
+        assertEquals("Business rule violated", response.getBody().getDescription());
+    }
+
+    @Test
+    void handleBusinessExceptionWithInvalidStatus() {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .title("Business Error")
+                .description("Business rule violated")
+                .status(999)
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
+        BusinessException businessException = new BusinessException(errorResponse);
+
+        ResponseEntity<ErrorResponse> response = handler.handleBusinessException(businessException);
+
+        assertEquals(500, response.getStatusCode().value());
+    }
+
+    @Test
+    void handleValidationExceptions() {
+        MethodArgumentNotValidException exception = mock(MethodArgumentNotValidException.class);
+        BindingResult bindingResult = mock(BindingResult.class);
+
+        List<ObjectError> errors = new ArrayList<>();
+        errors.add(new FieldError("object", "field", null, false, new String[]{"NotNull"}, null, "Field must not be null"));
+
+        when(exception.getBindingResult()).thenReturn(bindingResult);
+        when(bindingResult.getAllErrors()).thenReturn(errors);
+
+        ResponseEntity<ErrorResponse> response = handler.handleValidationExceptions(exception);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Erro na validação de campos", response.getBody().getTitle());
+        assertEquals("Field must not be null", response.getBody().getDescription());
+    }
+
+    @Test
+    void mapError() {
+        ObjectError error = new FieldError("object", "field", null, false, new String[]{"NotNull"}, null, "Field must not be null");
+        Map<String, ErrorResponse> errors = new HashMap<>();
+
+        handler.mapError(error, errors);
+
+        assertTrue(errors.containsKey("NotNull"));
+        assertEquals("Erro na validação de campos", errors.get("NotNull").getTitle());
+        assertEquals("Field must not be null", errors.get("NotNull").getDescription());
+        assertEquals(HttpStatus.BAD_REQUEST.value(), errors.get("NotNull").getStatus());
+        assertNotNull(errors.get("NotNull").getTimestamp());
+    }
+
+    @Test
+    void returnError_WithNotNull() {
+        Map<String, ErrorResponse> errors = new HashMap<>();
+
+        ErrorResponse notNullError = ErrorResponse.builder()
+                .title("Erro na validação de campos")
+                .description("Field must not be null")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
+        errors.put("NotNull", notNullError);
+
+        ErrorResponse result = handler.returnError(errors);
+
+        assertEquals(notNullError, result);
+    }
+
+    @Test
+    void returnError_WithNotNullAndNotEmptyAndNotBlank() {
+        Map<String, ErrorResponse> errors = new HashMap<>();
+
+        ErrorResponse notNullError = ErrorResponse.builder()
+                .title("Erro na validação de campos")
+                .description("Field must not be null")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
+        ErrorResponse notEmptyError = ErrorResponse.builder()
+                .title("Erro na validação de campos")
+                .description("Field must not be empty")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
+        ErrorResponse notBlankError = ErrorResponse.builder()
+                .title("Erro na validação de campos")
+                .description("Field must not be blank")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
+        errors.put("NotNull", notNullError);
+        errors.put("NotEmpty", notEmptyError);
+        errors.put("NotBlank", notBlankError);
+
+        ErrorResponse result = handler.returnError(errors);
+
+        assertEquals(notNullError, result);
+    }
+
+    @Test
+    void returnError_WithNotEmptyAndNotBlank() {
+        Map<String, ErrorResponse> errors = new HashMap<>();
+
+        ErrorResponse notEmptyError = ErrorResponse.builder()
+                .title("Erro na validação de campos")
+                .description("Field must not be empty")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
+        ErrorResponse notBlankError = ErrorResponse.builder()
+                .title("Erro na validação de campos")
+                .description("Field must not be blank")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
+        errors.put("NotEmpty", notEmptyError);
+        errors.put("NotBlank", notBlankError);
+
+        ErrorResponse result = handler.returnError(errors);
+
+        assertEquals(notEmptyError, result);
+    }
+
+    @Test
+    void returnError_WithNotBlank() {
+        Map<String, ErrorResponse> errors = new HashMap<>();
+
+        ErrorResponse notBlankError = ErrorResponse.builder()
+                .title("Erro na validação de campos")
+                .description("Field must not be blank")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
+        errors.put("NotBlank", notBlankError);
+
+        ErrorResponse result = handler.returnError(errors);
+
+        assertEquals(notBlankError, result);
+    }
+
+    @Test
+    void returnError_Empty() {
+        Map<String, ErrorResponse> errors = new HashMap<>();
+
+        ErrorResponse result = handler.returnError(errors);
+
+        assertNotNull(result);
+    }
+}
